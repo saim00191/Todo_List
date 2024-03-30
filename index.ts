16b89e0317be8b283545d84c97e64462d15b6727
saim00191 <saimamjad198@gmail.com>
2024-03-31 00:43:39 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+
+
+import inquirer from "inquirer";
+
+let todos = [];
+let again = true;
+
+while (again){
+
+const answers = await inquirer.prompt([
+    {
+        type : "input",
+        name : "todos",
+        message : "What do you want to add in your Todos?"
+    },
+    {
+        type : 'confirm',
+        name : "addTodos",
+        message : "Do you want to add more Todos?"
+    }
+]);
+todos.push(answers.todos);
+again = answers.addTodos;
+console.log(todos);
+
+}
